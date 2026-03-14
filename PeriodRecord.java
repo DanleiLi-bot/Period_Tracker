741e5bd3e3c777df70a5b6870c0a30c47326928a
Danlei <dli332@aucklanduni.ac.nz>
2026-03-14 21:27:48 +1300

    Constructing the period record code to record one incident of period

--- PeriodRecord.java
+++ PeriodRecord.java
@@ -1,3 +1,34 @@
+/*
+This class represents a record of a menstruration period,
+including start date, duration and cycle length.
+*/
+
+import java.time.LocalDate;
+
 public class PeriodRecord {
+    private LocalDate startDate; //Start date
+
+    public LocalDate getStartDate(){ //Getter for start date of the period
+        return startDate;
+    }
+
+    private int duration; //Duration
+
+    public int getDuration(){ //getter for duration of the period
+        return duration;
+    }
+    private int cycleLength; //cycle length
+
+    public int getCycleLength(){ //getter for cycle length of the period
+        return cycleLength;
+    }
+
+    public PeriodRecord(LocalDate startDate, int duration, int cycleLength) { //Constructor for the period record
+        this.startDate = startDate;
+        this.duration = duration;
+        this.cycleLength = cycleLength;
+    }
+
+
     
 }
