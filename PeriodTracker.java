304783cd434276f4c442b6959c6c682c64ef1755
Danlei <dli332@aucklanduni.ac.nz>
2026-03-14 22:45:51 +1300

    Construction of the PeriodTracker to store records and to predict the start date of the next period

--- PeriodTracker.java
+++ PeriodTracker.java
@@ -1,3 +1,36 @@
+
+/*
+This class is responsible for tracking the period records and predicting the startdate of the next period
+*/
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class PeriodTracker {
-    
+    private ArrayList<PeriodRecord> records; //Creating the list for records of periods
+
+    public PeriodTracker() { //Constructing the records as array list
+        records = new ArrayList<>();
+    }
+
+    public void addPeriodRecord(PeriodRecord record){ //adding the input record into the list
+        records.add(record);
+    }
+
+    public LocalDate predictionDate(){ //Predicting next period start date
+        LocalDate prediction; 
+        
+        if (records.size()==0){ //returning null if there is no data
+            return null;
+        }
+
+        int currentLength = records.size();
+        PeriodRecord lastOne = records.get(currentLength - 1);
+        LocalDate lastDate = lastOne.getStartDate();
+        int lastCycle = lastOne.getCycleLength();
+        prediction = lastDate.plusDays(lastCycle); //Predicting using the last period record data
+
+        return prediction;
+    }
 }
+
